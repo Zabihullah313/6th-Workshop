fbd67ec6fdae7112d2d1ea2971ad1413527daf8d
Zabihullah <zabihullahmohammadi313@gmail.com>
2024-11-13 00:08:48 +0330

    At the end we make the class's objects and a list for them to print the details of that objects list and test the action of hunting.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        Cheetah cheetah = new Cheetah("Cheetah", 12, 120);
+        Parrot parrot = new Parrot("Parrot", 3, 50);
+        Giraffe giraffe = new Giraffe("Giraffe", 8, 25);
+        Eagle eagle = new Eagle("Eagle", 5, 200);
+
+
+        List<Animal> zooAnimals = new ArrayList<>();
+        zooAnimals.add(cheetah);
+        zooAnimals.add(parrot);
+        zooAnimals.add(giraffe);
+        zooAnimals.add(eagle);
+
+        // Displaying animal details
+        for (Animal animal : zooAnimals) {
+            animal.show();
+        }
+
+        //Hunting behavior
+        cheetah.hunt(parrot);
+        eagle.hunt(giraffe);
+
 
     }
 }
